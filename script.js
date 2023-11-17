59516ce693b03ea631b29d737d209956572e1568
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-17 16:37:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const parseCode = (str) => {
   // your code here
+	    let arr1 = str.split('0');
+      let arr = [];
+      for(let i = 0; i < arr1.length; i++ ){
+        if(arr1[i] != ''){
+            arr.push(arr1[i]);
+        }
+      }
+      let obj = {
+        firstName: arr[0],
+        lastName: arr[1],
+        id: arr[2]
+      };
+      
+      return obj;
 };
 
 // Do not change the code below
